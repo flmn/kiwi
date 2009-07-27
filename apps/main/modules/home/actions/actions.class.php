f3f0fadff8b187d735c755f1b6d300c879ad992c
jitao <jitao@jitao-ubuntu.(none)>
2009-07-27 19:30:51 +0800

    initial code commit

---
+++ apps/main/modules/home/actions/actions.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * home actions.
+ *
+ * @package    kiwi
+ * @subpackage home
+ * @author     jitao
+ */
+class homeActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    ;
+  }
+}
