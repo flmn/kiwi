000063d5d18d35aaeea8c313fc85cd1bbd1c45e5
jitao <flmnix@gmail.com>
2009-08-03 11:34:25 +0800

    rename module from user to adminUser

---
+++ apps/main/modules/adminProject/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * project actions.
+ *
+ * @package    kiwi
+ * @subpackage project
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 12479 2008-10-31 10:54:40Z fabien $
+ */
+class adminProjectActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
